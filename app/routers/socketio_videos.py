10c211b05e2bd536cb57b564b040294b33586f88
PamelaKM26 <pamelamc23@gmail.com>
2025-04-09 22:12:29 -0500

    modificaciones socketio

--- app/routers/socketio_videos.py
+++ app/routers/socketio_videos.py
@@ -8,8 +8,12 @@ from fastapi import APIRouter
 from fastapi.encoders import jsonable_encoder
 import asyncio
 
+# sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
+# sio_app = socketio.ASGIApp(sio)
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="http://localhost:4200")
-sio_app = socketio.ASGIApp(sio)
+# sio_app = socketio.ASGIApp(sio, socketio_path="/socket.io")
+sio_app = socketio.ASGIApp(sio, socketio_path="/ws/videos/socket.io")
+
 router = APIRouter()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -25,13 +29,26 @@ async def disconnect(sid):
 async def broadcast_videos():
     db = SessionLocal()
     try:
+        print("Obteniendo videos de la base de datos...")
         videos = db.query(Videos).order_by(Videos.order).all()
+
+        if not videos:
+            print("No se encontraron videos en la base de datos.")
+        else:
+            print(f"Videos obtenidos: {videos}")
+
         data = [jsonable_encoder(VideoSchema.model_validate(video)) for video in videos]
+                # Emitir los datos a los clientes conectados
+        print(f"Emitiendo videos: {data}")  # Esto es útil para depuración
+        await sio.emit("videos", data)
+
+    except Exception as e:
+        print(f"Error al obtener o emitir los videos: {e}")
     finally:
         db.close()
-    await sio.emit("videos", data)
 
 @router.get("/broadcast")
 async def trigger_video_broadcast():
+    print("Triggering broadcast...")
     await broadcast_videos()
     return {"message": "Broadcast videos realizado"}
